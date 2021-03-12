643c5f97b7ef2d7dd0a9d6c50a42253c44bdab6c
KHANH PHAN HUNG <KHANH.PH184277@sis.hust.edu.vn>
2021-03-12 14:05:03 +0700

    Add Lab03

--- Lab01/ex6_4.java
+++ Lab01/ex6_4.java
@@ -6,21 +6,21 @@ public class ex6_4 {
 		int a=0;
 		System.out.println("Enter the month: ");
 		String str=sc.nextLine();
-		if(str=="January"||str=="Jan."||str=="Jan"||str=="1") a=1;
-		if(str=="Februaly"||str=="Feb."||str=="Feb"||str=="2")  a=2;
-		if(str=="March"||str=="Mar."||str=="Mar"||str=="3") a=3;
-		if(str=="April"||str=="Apr."||str=="Apr"||str=="4") a=4;
-		if(str=="May"||str=="5") a=5;
-		if(str=="June"||str=="Jun"||str=="6") a=6;
-		if(str=="July"||str=="Jul"||str=="7") a=7;
-		if(str=="August"||str=="Aug."||str=="Aug"||str=="8") a=8;
-		if(str=="September"||str=="Sept."||str=="Sep"||str=="9") a=9;
-		if(str=="October"||str=="Oct."||str=="Oct"||str=="10") a=10;
-		if(str=="November"||str=="Nov."||str=="Nov"||str=="11") a=11;
-		if(str=="December"||str=="Dec."||str=="Dec"||str=="12") a=12;
+		if(str.equals("January")||str.equals("Jan.")||str.equals("Jan")||str.equals("1")) a=1;
+		if(str.equals("Februaly")||str.equals("Feb.")||str.equals("Feb")||str.equals("2"))  {a=2;}
+		if(str.equals("March")||str.equals("Mar.")||str.equals("Mar")||str.equals("3")) a=3;
+		if(str.equals("April")||str.equals("Apr.")||str.equals("Apr")||str.equals("4")) a=4;
+		if(str.equals("May")||str.equals("5")) a=5;
+		if(str.equals("June")||str.equals("Jun")||str.equals("6")) a=6;
+		if(str.equals("July")||str.equals("Jul")||str.equals("7")) a=7;
+		if(str.equals("August")||str.equals("Aug.")||str.equals("Aug")||str.equals("8")) a=8;
+		if(str.equals("September")||str.equals("Sept.")||str.equals("Sep")||str.equals("9")) a=9;
+		if(str.equals("October")||str.equals("Oct.")||str.equals("Oct")||str.equals("10")) a=10;
+		if(str.equals("November")||str.equals("Nov.")||str.equals("Nov")||str.equals("11")) a=11;
+		if(str.equals("December")||str.equals("Dec.")||str.equals("Dec")||str.equals("12")) a=12;
 		
 		System.out.println("Enter the year: ");int y=sc.nextInt();
-		//System.out.println(a);
+		System.out.println(a+str);
 		switch(a)
 		{
 		case 1:case 3: case 5: case 7: case 8: case 10: case 12: {System.out.println(a+"/"+y+" have 31 days");break;}
---
+++ Lab03/Cart.java
@@ -0,0 +1,45 @@
+import java.lang.Object;
+public class Cart {
+	private int qtyOrdered;
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemsOrdered[]= new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc disc)
+	{
+		if(qtyOrdered==20) System.out.println("The cart is almost full !");
+		else
+		{
+			qtyOrdered++;
+			itemsOrdered[qtyOrdered].setTitle(disc.getTitle());
+			itemsOrdered[qtyOrdered].setCategory(disc.getCategory());
+			itemsOrdered[qtyOrdered].setDirector(disc.getDirector());
+			itemsOrdered[qtyOrdered].setLength(disc.getLength());
+			itemsOrdered[qtyOrdered].setCost(disc.getCost());
+			System.out.println( "The disc has been added !");
+		}
+	}
+	
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc)
+	{
+		for(int i=0;i<qtyOrdered;i++)
+		{
+			if(disc.getTitle().equals(itemsOrdered[qtyOrdered].getTitle()) && disc.getDirector().equals(itemsOrdered[qtyOrdered].getDirector()) ) 
+			{
+				for(int j=i;j<qtyOrdered;j++)
+				{
+					j=j++;
+				}
+				System.out.println("The disc have been deleted !");
+			}else System.out.println("Have no this disc");
+		}
+	}
+	public float totalCost()
+	{
+		float cost=0;
+		for(int i=0;i<qtyOrdered;i++)
+		{
+			cost+=itemsOrdered[qtyOrdered].getCost();
+		}
+		return cost;
+	}
+}
